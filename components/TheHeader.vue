8aca57b0e52a1360d4f7f4b44d07c30ed9e85ff0
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2022-11-21 13:50:47 +0500

    default layout

---
+++ components/TheHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+   
+  </header>
+</template>
+
+<!--<script lang="ts">
+import { Component, Vue } from "nuxt-property-decorator";
+import { vmx } from "~/store/store.vuex";
+
+@Component
+export default class TheHeader extends Vue {
+   public cars: TrimUniqItem[] = [];
+
+
+  async mounted() {
+    console.log('ТВт')
+    await vmx.trim.fetchTrim(1);
+    const trims = vmx.trim.getTrim;
+    await vmx.car.fetchCarById(trims);
+    this.cars = vmx.car.getCar
+  }
+
+  async fetchCars(value) {
+   await vmx.car.fetchCarById(value)
+   this.cars = vmx.car.getCar
+    console.log('Header')
+  }
+};
+</script>-->
